33e1ad86d5639f2055edc49566953bbec56912d8
donmaicha <genmaicha77783@gmail.com>
2020-03-20 22:51:33 +0900

    viewを作ってみた

--- sample/Controllers/HelloWorldController.cs
+++ sample/Controllers/HelloWorldController.cs
@@ -11,10 +11,9 @@ namespace sample.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action";
+            return View();
         }
 
         // 
